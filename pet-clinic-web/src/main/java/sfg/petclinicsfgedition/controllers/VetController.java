4fa93b4699172ad14d03699a6f343fed063c353c
adi forkasiewicz <adiforka@gmail.com>
2020-01-21 19:24:42 +0100

    List all vets on Vet Index Page. Closes #23

--- pet-clinic-web/src/main/java/sfg/petclinicsfgedition/controllers/VetController.java
+++ pet-clinic-web/src/main/java/sfg/petclinicsfgedition/controllers/VetController.java
@@ -1,14 +1,26 @@
 package sfg.petclinicsfgedition.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sfg.petclinicsfgedition.services.VetService;
 
 @Controller
 public class VetController {
 
-    //this is options for user input convenience
+    private VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets", "vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
 
         //this returns a view in mvc controllers
         return "vets/index";
