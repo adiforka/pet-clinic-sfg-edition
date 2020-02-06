6c4de13e876b087becb42da4404f5970d3d5e663
adi forkasiewicz <adiforka@gmail.com>
2020-02-06 22:55:39 +0100

    Implemented PetTypeService interface and PetTypeMapService class. Closes #38

---
+++ pet-clinic-data/src/main/java/sfg/petclinicsfgedition/services/map/PetTypeService.java
@@ -0,0 +1,7 @@
+package sfg.petclinicsfgedition.services.map;
+
+import sfg.petclinicsfgedition.model.PetType;
+import sfg.petclinicsfgedition.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
