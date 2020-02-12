cbd25cd378cfaefa874f1ecde1dd908b4f7e0bf4
adi forkasiewicz <adiforka@gmail.com>
2020-02-12 16:47:53 +0100

    Converted Owner and related classes to JPA entities with mappings. Closes #43

--- pet-clinic-data/src/main/java/sfg/petclinicsfgedition/model/Owner.java
+++ pet-clinic-data/src/main/java/sfg/petclinicsfgedition/model/Owner.java
@@ -1,14 +1,26 @@
 package sfg.petclinicsfgedition.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+//now it'll be a table of owners in the db
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+
     //initializing this here for now so we don't get an NPE when we try to add pets to the set in DataLoader
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
--- pet-clinic-data/src/main/java/sfg/petclinicsfgedition/model/Person.java
+++ pet-clinic-data/src/main/java/sfg/petclinicsfgedition/model/Person.java
@@ -1,7 +1,17 @@
 package sfg.petclinicsfgedition.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
+
+    /*we don't want a person object in db, but we do want its properties as inherited by Vet, Owner etc. as column names
+    on those objects*/
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
