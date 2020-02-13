6e407a223583c8a610bf9cfbe83fd27dccb5cc1b
adi forkasiewicz <adiforka@gmail.com>
2020-02-13 16:37:07 +0100

    Created Visit Entity with relationship mapping to Pet (interesting difference with reference app, which has a relationship of visit to petId property) Closes#45

--- pet-clinic-data/src/main/java/sfg/petclinicsfgedition/model/Visit.java
+++ pet-clinic-data/src/main/java/sfg/petclinicsfgedition/model/Visit.java
@@ -1,12 +1,26 @@
 package sfg.petclinicsfgedition.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity  {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_visit")
     private Pet pet;
+
+
+    public Visit() {
+        this.date = LocalDate.now();
+    }
 
 
     public LocalDate getDate() {
